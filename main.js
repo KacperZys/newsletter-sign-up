0463d4f5f1efc7e835cc44b40bd2a87646a35c17
Kacper Zys <kacperzys13@gmail.com>
2024-01-27 21:06:59 +0100

    added validation and success message
    added validation in js and new success window to show after entering email

---
+++ main.js
@@ -0,0 +1,45 @@
+const button = document.querySelector('.form__button');
+const input = document.querySelector('.form__input');
+const form = document.querySelector('.form');
+const success = document.querySelector('.success');
+const main_article = document.querySelector('.main_article');
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+button.addEventListener('click', e => {
+    let confirm = false;
+    const value = input.value;
+    const validation = document.querySelector('.validation');
+
+
+    if (value.length > 0) {
+        validation.innerText = '';
+
+        if (value.match(validRegex)) {
+            confirm = true;
+        }
+
+        if (!confirm) {
+            input.style.border = '2px solid red';
+            validation.innerText = 'Invalid email';
+        }
+        else {
+            input.style.border = '2px solid gray';
+            validation.innerText = '';
+            main_article.style.visibility = 'hidden';
+            success.style.visibility = 'visible';
+
+            document.querySelector('.email').innerText = value;
+        }
+    } else {
+        validation.innerText = 'The field is left empty';
+        input.style.border = '2px solid red';
+    }
+});
+
+
+const dismiss = document.querySelector('.success__button');
+
+dismiss.addEventListener('click', e => {
+    success.style.visibility = 'hidden';
+    main_article.style.visibility = 'visible';
+});
